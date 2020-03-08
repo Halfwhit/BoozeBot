881864c4c8e99f61fe114909e7f8cbf02e4c1676
Halfwhit <halfwhit@gmail.com>
2020-03-08 17:33:50 +0000

    Added bot file

---
+++ boozebot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# boozebot.py
+import discord
+
+# Token taken from developer portal. Should move this to another file.
+api_token = "NDk3NDMxMzkyNzg4MTUyMzUw.XmJsOQ.MbZACKmWlGbGHyRjRfF8K6Mdd1k"
+
+# Setup the client (A client is a low level bot, so basically initialise the bot)
+client = discord.Client()
+
+# Event callbacks the bot will use
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'F':
+        await message.channel.send("F")
+
+    if 'RIP' in message.content:
+        await message.add_reaction("\U0001F1EB")
+
+    if message.content == '!ping':
+        await message.channel.send("pong!")
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello {0.author.mention}'.format(message))
+
+# Start the bot and start listening for events
+client.run(api_token)
